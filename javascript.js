5b870ccdaab5e7405fb7b5114d86256f4b67c90e
Cody kelderman <codykelderman@gmail.com>
2023-06-09 12:49:27 -0600

    create card for each display

--- javascript.js
+++ javascript.js
@@ -1,7 +1,12 @@
 const addBtn = document.querySelector('#addButton');
+const newBtn = document.querySelector('.newButton');
+const cardDisplay = document.querySelector('.cardDisplay');
 
-
-let myBooks = [];
+let myBooks = [
+  book1 = {title: 'Pickle', author: 'John', pages: 3},
+  book2 = {title: 'Sandy', author: 'Bill', pages: 22},
+  book3 = {title: 'What', author: 'eric', pages: 22}
+];
 
 function Book(title, author, pages) {
     this.title = title
@@ -21,3 +26,28 @@ addBtn.addEventListener('click', function (event) {
   event.preventDefault();
   addBookToLibrary();
 });
+
+newBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  myBooks.forEach(createCards)
+})
+
+
+function createCards(book) {
+  const bookDiv = document.createElement('div');
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+
+  bookDiv.classList.add('book-card');
+
+  title.textContent = `'${book.title}'`
+  author.textContent = `'${book.author}'`
+  pages.textContent = `'${book.pages}'`
+
+
+  cardDisplay.appendChild(bookDiv);
+  bookDiv.appendChild(title);
+  bookDiv.appendChild(author);
+  bookDiv.appendChild(pages);
+}
