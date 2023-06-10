33d0d27cc6a20774c02c74f6fc2f72f8b6ffbcae
Cody kelderman <codykelderman@gmail.com>
2023-06-10 10:53:46 -0700

    add read status to book additions

--- javascript.js
+++ javascript.js
@@ -8,17 +8,24 @@ let myBooks = [
   book3 = {title: 'What', author: 'eric', pages: 22}
 ];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
+    this.read = read
 };
 
 function addBookToLibrary() {
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let pages = document.querySelector("#pages").value;
-    let book = new Book(title, author, pages);
+    let read;
+    if (document.querySelector("#readStatus").checked === true) {
+      read = 'yes';
+    } else {
+      read = 'no';
+    };
+    let book = new Book(title, author, pages, read);
     myBooks.push(book);
  };
 
